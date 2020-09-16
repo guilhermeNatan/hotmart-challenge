170ea91b842e4c5c7382ca0c72aefb9bccb34e2b
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2020-09-16 08:24:53 -0300

    first commit

---
+++ src/main/java/com/desafio/hotmart/HotmartApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.hotmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/hotmart/HotmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.hotmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
